ae52b81119754944cdf816a0c6482ba4ec8aa584
cuongnvt <cuongnvt@rikkeisoft.com>
2020-01-10 08:44:43 +0700

    Init project

---
+++ ShorternUrl/ShorternUrl/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShorternUrl
+//
+//  Created by CuongNVT on 1/10/20.
+//  Copyright © 2020 CuongNVT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
